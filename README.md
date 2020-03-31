78601b8aa82b39495c149da522b8671e726b2882
darma <1946498212@qq.com>
2020-03-31 22:33:57 +0800

    扩充高度自适应，修复非正方形图片比例失调

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 PHP九宫格分图(将一张图分成9张，并保存)
 # PicCuter->set_save_dir(dir)
 设置9张图片保存的文件夹(相对路径),默认为当前文件夹
-# PicCuter->cut_picture($src_path,$fpixel)
+# PicCuter->cut_picture($src_path,$width)
 $src_path:图片地址(本地图片，网络图片无效)
-$fpixel:裁剪后单张图片的宽高(正方形)默认为250px
-
+$width:裁剪后单张图片的宽默认为250px 高度会按原图比例自适应
--- src/PicCuter.php
+++ src/PicCuter.php
@@ -9,15 +9,22 @@ protected $filename;
 //图片的长宽
 protected $img_info;
 //切割后单个图片的长宽，默认250px
-protected $imgfpixel;
+// protected $imgfpixel;
+protected $img_height_fpixel;
+//切割后单个图片的宽
+protected $img_width_fpixel;
 public function set_save_dir($dir = ''){
     $this->save_dir = $dir;
 }
-public function cut_picture($src_path,$fpixel = 250){
+public function cut_picture($src_path,$fpixel_width = 250){
     //end()要传递一个定义的变量，不然会报警告
     //name为图片文件的名字
     $ar = explode('/', $src_path);
     $name = end($ar);
+    $this->img_info = getimagesize($src_path);
+    $this->img_width_fpixel = $fpixel_width;
+    $this->img_height_fpixel = $this->img_info[1]/$this->img_info[0]*$this->img_width_fpixel;
+    
     //使用某些插件截取图片后上传会出现检测不到扩展名的情况，故默认jpg
     if(!pathinfo($name)['extension']){
         $this->ext = 'jpg';     
@@ -25,10 +32,8 @@ public function cut_picture($src_path,$fpixel = 250){
     else{
     $this->ext = pathinfo($name)['extension'];
     }
-    $this->imgfpixel = $fpixel;
     $this->ext = pathinfo($name)['extension'];
     $this->filename = pathinfo($name)['filename'];
-    $this->img_info = getimagesize($src_path);
     $ans = $this->cut($src_path);
     return $ans;
 }
@@ -38,15 +43,16 @@ private function cut($src_path){
     $pic_src = array();
     $width = $this->img_info[0]/3;
     $height = $this->img_info[1]/3;
-    $fpx = $this->imgfpixel;
+    $width_fpx = $this->img_width_fpixel;
+    $height_fpx = $this->img_height_fpixel;
     $src = imagecreatefromstring(file_get_contents($src_path));
     //将裁剪区域复制到新图片上，并根据源和目标的宽高进行缩放或者拉升
-    $new_image = imagecreatetruecolor($fpx, $fpx);
+    $new_image = imagecreatetruecolor($width_fpx, $height_fpx);
     for ($y=0; $y <3 ; $y++) { 
     for ($x=0; $x <3 ; $x++) { 
         $new_x = $x*$width;
         $new_y = $y*$height;   
-    imagecopyresampled($new_image, $src, 0, 0, $new_x, $new_y, $this->imgfpixel, $this->imgfpixel, $width, $height);
+    imagecopyresampled($new_image, $src, 0, 0, $new_x, $new_y, $width_fpx, $height_fpx, $width, $height);
     //记录分割的图片
     $p_src = $this->save_dir.'/'.$this->filename.'_'.$num.'.'.$this->ext;
     //保存图片
